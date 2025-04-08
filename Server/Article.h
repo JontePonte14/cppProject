92c64f5ee9647e13a0395791e5162e9a9d7c965c
Jonathan <jonathan.vonbergen@gmail.com>
2025-04-08 14:50:25 +0200

    Påbörjat med implemnteation av article header class

--- Server/Article.h
+++ Server/Article.h
@@ -1,31 +1,31 @@
-#ifndef INTERFACE_H
-#define INTERFACE_H
+#ifndef ARTICLE_H
+#define ARTICLE_H
 #include <string>
 
 class Article
 {
+public:
+    Article() = default;
+    Article(const std::string title, 
+        const std::string author, 
+        const std::string date, // Format: year-month-day
+        int id, 
+        const std::string body);
+    ~Article() = default;
+
+    Article getArticle() const;
+    std::string getArticleName() const;
+    std::string getArticleDate() const;
+    int getArticleID() const;
+
 private:
     std::string title;
     std::string author;
     std::string date; // Format: year-month-day
     int idNbr;
     std::string body;
-public:
-    Article() = default;
-    Article(std::string t, std::string a, std::string d, int id, std::string b);
-    ~Article();
+
     
 };
 
-Article::Article(/* args */)
-
-
-{
-}
-
-Article::~Article()
-
-
-{
-}
 #endif
