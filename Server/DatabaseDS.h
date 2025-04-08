ad36107ed83c4c83d0a33607a510a612a018c992
Jonathan <jonathan.vonbergen@gmail.com>
2025-04-08 14:00:21 +0200

    La till samma funktioner  som i interface

--- Server/DatabaseDS.h
+++ Server/DatabaseDS.h
@@ -1,8 +1,20 @@
+#include "Article.h"
+#include <string>
+#include <list>
+
 class DatabaseDS
 {
 private:
     /* data */
 public:
+    std::string listGroup();
+    bool makeGroup();
+    bool removeGroup();
+
+    std::list<Article> listArticle();
+    bool makeArticle();
+    bool removeArticle();
+    Article getArticle();
     DatabaseDS(/* args */);
     ~DatabaseDS();
 };
