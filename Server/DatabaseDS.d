4cc51f0505082bac66dfa72913dcf0115b81ee97
Jonathan <jonathan.vonbergen@gmail.com>
2025-04-09 09:40:15 +0200

    Undersöker hur filesystem fungerar, glömde städa från make

--- Server/DatabaseDS.d
+++
@@ -1,3 +0,0 @@
-DatabaseDS.o: DatabaseDS.cpp DatabaseDS.h Article.h
-DatabaseDS.h:
-Article.h:
--- Server/TestDatabaseDS.d
+++
@@ -1,3 +0,0 @@
-TestDatabaseDS.o: TestDatabaseDS.cpp DatabaseDS.h Article.h
-DatabaseDS.h:
-Article.h:
