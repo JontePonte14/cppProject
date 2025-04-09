eede8b52d6b37ff745cd018337d73984031d3a6b
Jonathan <jonathan.vonbergen@gmail.com>
2025-04-09 15:36:48 +0200

    Städe från make

--- Server/DatabaseDS.d
+++
@@ -1,4 +0,0 @@
-DatabaseDS.o: DatabaseDS.cpp DatabaseDS.h ../external/json.hpp Article.h
-DatabaseDS.h:
-../external/json.hpp:
-Article.h:
--- Server/TestDatabaseDS.d
+++
@@ -1,5 +0,0 @@
-TestDatabaseDS.o: TestDatabaseDS.cpp DatabaseDS.h ../external/json.hpp \
- Article.h
-DatabaseDS.h:
-../external/json.hpp:
-Article.h:
