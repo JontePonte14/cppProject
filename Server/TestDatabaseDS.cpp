e49cb584a8fd2b248a0023fb3955c000b706066b
Jonathan <jonathan.vonbergen@gmail.com>
2025-04-09 09:10:40 +0200

    Påbörjat test class för DS

---
+++ Server/TestDatabaseDS.cpp
@@ -0,0 +1,15 @@
+#include "DatabaseDS.h"
+#include "Article.h"
+#include <string>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(){
+
+    DatabaseDS DS();
+    cout << DS.listGroup() << endl;
+
+
+}
